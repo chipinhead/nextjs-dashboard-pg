af6398b210ffd81f135cdbf81a5ad96fa1d9ce72
Juni Samos <juni@Junis-MacBook-Pro.local>
2024-09-08 14:04:29 +0200

    Chapter 14

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,14 +1,14 @@
-import { Pool, QueryResult } from 'pg';
+import { Pool, QueryResult, QueryConfig } from 'pg';
 
 // Create a new pool
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
 });
 
-export async function query(text: string, params?: any[]): Promise<QueryResult>{
+export async function query<T extends QueryResultRow>(text: string, params?: QueryConfig['values']): Promise<QueryResult<T>> {
   const client = await pool.connect();
   try {
-    return await client.query(text, params);
+    return await client.query<T>(text, params);
   } finally {
     client.release();
   }
